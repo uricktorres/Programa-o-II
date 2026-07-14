83a94746549b93e526111942fbe84ed466a15690
uricktorres <148587376+uricktorres@users.noreply.github.com>
2026-07-13 22:15:05 -0300

    Update Ex1-2.cpp

--- Atividades/Ex1-2.cpp
+++ Atividades/Ex1-2.cpp
@@ -6,8 +6,8 @@ int main() {
     string nome;
     int idade;
 
-    getline(cin, nome);   // lê o nome completo (com espaços)[web:18][web:29]
-    cin >> idade;         // lê a idade como inteiro[web:31]
+    getline(cin, nome);   // lê o nome completo 
+    cin >> idade;         // lê a idade como inteiro
 
     cout << nome << endl;
     cout << idade << endl;
