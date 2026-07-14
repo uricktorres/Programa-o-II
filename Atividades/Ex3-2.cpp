de6afaf570c235b6df7c5046b1bb7cc8394bf46b
uricktorres <148587376+uricktorres@users.noreply.github.com>
2026-07-13 22:16:33 -0300

    Add Ex3-2.cpp for input and output of three doubles

---
+++ Atividades/Ex3-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b, c;
+
+    cin >> a >> b >> c;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+
+    return 0;
+}
